409e03a39257223c2258c1b0d81d1d97d3759db6
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-03-12 14:45:25 +0300

    add changes to jQuery

---
+++ js/delani.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".design").click(function(){
+        $(".p").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $(".development").click(function(){
+        $(".p1").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $(".product").click(function(){
+        $(".p2").toggle();
+    });
+});
